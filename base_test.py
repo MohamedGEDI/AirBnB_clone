617043f5323f505736b662f3a4a8738e36563122
MohamedGEDI <gedimohammed@gmail.com>
2021-11-09 19:16:29 +0300

    -Task 1 make BaseModel

---
+++ base_test.py
@@ -0,0 +1,17 @@
+
+from models.base_model import BaseModel
+import time
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    time.sleep(3.00)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,37 @@
 #!/usr/bin/python3
+"""import unique id and date time"""
+
 
 import uuid
+import datetime
+
+"""Create a BaseModel class"""
+
+
 class BaseModel:
-    def __init__(self, id=None):
+    """Constructor to contain public instances"""
+    def __init__(self):
+        """id, created_at, updated_at instances created"""
         id = str(uuid.uuid4())
         self.id = id
+        created_at = datetime.datetime.now()
+        created_at = str(created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"))
+        self.created_at = created_at
+        self.updated_at = created_at
+
+    def save(self):
+        """update time when saving"""
+        updated_at = datetime.datetime.now()
+        updated_at = str(updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"))
+        self.updated_at = updated_at
+        return self.updated_at
+
+    def to_dict(self):
+        """return dictionary of class for json"""
+        dict1 = self.__dict__
+        dict1["__class__"] = self.__class__.__name__
+        return dict1
+
     def __str__(self):
+        """return print out"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
